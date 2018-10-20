9e434bffecfe69ac18f7d42b63ec474287cf7910
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-20 17:39:22 +0100

    Generate migration for state, city and geopolitical models

---
+++ database/migrations/2018_10_20_160844_create_geopolitical_zones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeopoliticalZonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('geopolitical_zones', function (Blueprint $table) {
+            $table->string('code');
+            $table->string('zone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('geopolitical_zones');
+    }
+}
---
+++ database/migrations/2018_10_20_162920_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('state_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('lat');
+            $table->string('lon');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
