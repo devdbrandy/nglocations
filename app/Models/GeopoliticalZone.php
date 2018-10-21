5459b81699f6e679d957220ebe08bb6df6e66f3f
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-21 16:35:59 +0100

    Add and setup Geopolitical model

--- app/Models/GeopoliticalZone.php
+++ app/Models/GeopoliticalZone.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class GeopoliticalZone extends Model
 {
-    //
+    /**
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'gp_zones';
+
+    /**
+     * The table default primary key
+     *
+     * @var string
+     */
+    protected $primaryKey = 'code';
+
+    /**
+     * Define the primary key type
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model should be timestamped
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
 }
