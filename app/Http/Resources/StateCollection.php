44a6c7d2009a4bb49f0f0b29a0aa1fb342973fdf
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-22 10:06:14 +0100

    Add support for capital provision via custom param

--- app/Http/Resources/StateCollection.php
+++ app/Http/Resources/StateCollection.php
@@ -19,6 +19,7 @@ class StateCollection extends JsonResource
             'code' => $this->code,
             'name' => $this->name,
             'alias' => $this->alias,
+            'capital' => $this->when($request->has('capital'), $this->capital),
             'href' => route('api.states.show', $this->alias),
             'lgas' => $this->when($request->has('lgas'), CityResource::collection($this->lgas)),
             'cities' => $this->when($request->has('cities'), CityResource::collection($this->cities)),
