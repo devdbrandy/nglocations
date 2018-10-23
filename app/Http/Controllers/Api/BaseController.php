c5a6b6f6e6748d631a383712fcbd86e3b637d95b
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-23 21:55:54 +0100

    Define api documentation following OpenAPI specification

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -4,6 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 
+/**
+ * @OA\Info(
+ *   version="1.0.0",
+ *   title="NGLocations API",
+ *   description="REST API that allows users to retrieve information of all states in Nigeria.",
+ *   @OA\Contact(
+ *     name="DaylicousSoft",
+ *     email="daylicious@gmail.com"
+ *   ),
+ *   @OA\License(
+ *     name="MIT",
+ *     url="https://opensource.org/licenses/MIT"
+ *   )
+ * )
+ *
+ * @OA\Server(
+ *   url=L5_SWAGGER_CONST_HOST,
+ *   description="Server url"
+ * )
+ *
+ */
 class BaseController extends Controller
 {
     //
--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -15,23 +15,58 @@ use Symfony\Component\HttpFoundation\Response;
 class Controller extends BaseController
 {
     /**
+     * @OA\Get(
+     *   path="/states",
+     *   tags={"Get list of states"},
+     *   summary="Get list of states",
+     *   description="Returns list of state object",
+     *   operationId="getStates",
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   )
+     * )
+     *
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function getStates()
     {
         return StateCollection::collection(State::all());
     }
 
     /**
+     * @OA\Get(
+     *   path="/states/{state}",
+     *   tags={"Get a single state"},
+     *   summary="Retrieve a single states",
+     *   description="Returns a state object",
+     *   operationId="getState",
+     *   @OA\Parameter(
+     *      name="state",
+     *      description="State alias",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(type="string")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   ),
+     *   @OA\Response(
+     *     response=404,
+     *     description="Resource not found"
+     *   )
+     * )
+     *
      * Display the specified resource.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\State  $state
      * @return \Illuminate\Http\Response
      */
-    public function state(Request $request, State $state)
+    public function getState(Request $request, State $state)
     {
         if ($request->has('capital')) {
             return response()->json($state->capital);
@@ -41,45 +76,118 @@ class Controller extends BaseController
     }
 
     /**
+     * @OA\Get(
+     *   path="/states/{state}/cities",
+     *   tags={"List Cities in a state"},
+     *   summary="List Cities in a state",
+     *   description="Returns list of City object",
+     *   operationId="getCities",
+     *   @OA\Parameter(
+     *      name="state",
+     *      description="State alias",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(type="string")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   )
+     * )
+     *
      * Display a listing of the resource.
      *
      * @param  \App\Models\State  $state
      * @return \Illuminate\Http\Response
      */
-    public function lgas(State $state)
-    {
-        return LgaResource::collection($state->lgas);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @param  \App\Models\State  $state
-     * @return \Illuminate\Http\Response
-     */
-    public function cities(State $state)
+    public function getCities(State $state)
     {
         return CityResource::collection($state->cities);
     }
 
     /**
+     * @OA\Get(
+     *   path="/states/{state}/lgas",
+     *   tags={"List Local Governament Areas in a state"},
+     *   summary="List Local Governament Areas in a state",
+     *   description="Returns list of LGA object",
+     *   operationId="getLgas",
+     *   @OA\Parameter(
+     *      name="state",
+     *      description="State alias",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(type="string")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   )
+     * )
+     *
+     * Display a listing of the resource.
+     *
+     * @param  \App\Models\State  $state
+     * @return \Illuminate\Http\Response
+     */
+    public function getLgas(State $state)
+    {
+        return LgaResource::collection($state->lgas);
+    }
+
+    /**
+     * @OA\Get(
+     *   path="/lgas",
+     *   tags={"Get list of local governemnt areas"},
+     *   summary="Get list of local governemnt areas",
+     *   description="Returns list of LGA object",
+     *   operationId="getLgasAll",
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   )
+     * )
+     *
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function localGovAreas()
+    public function getLgasAll()
     {
         return LgaCollection::collection(LGA::all());
     }
 
     /**
+     * @OA\Get(
+     *   path="/lgas/{lga}",
+     *   tags={"Get a single Local Government Area"},
+     *   summary="Retrieve a single Local Government Area",
+     *   description="Returns a LGA object",
+     *   operationId="getLga",
+     *   @OA\Parameter(
+     *      name="lga",
+     *      description="Lga alias",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(type="string")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Successful operation"
+     *   ),
+     *   @OA\Response(
+     *     response=404,
+     *     description="Resource not found"
+     *   )
+     * )
+     *
      * Display the specified resource.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\LGA  $state
      * @return \Illuminate\Http\Response
      */
-    public function showLga(Request $request, LGA $lga)
+    public function getLga(Request $request, LGA $lga)
     {
         return new LgaResource($lga);
     }
